4949f479690aad3626893f349196417356b6b759
Min <solunar@vip.qq.com>
2024-03-05 21:34:49 +0800

    feat: 添加测试点位

--- packages/ui/src/map-annotation/index.vue
+++ packages/ui/src/map-annotation/index.vue
@@ -1,15 +1,23 @@
-<script setup>
+<script lang="ts" setup>
 import { onMounted } from 'vue';
-import { T, MarkerIconPrimary } from '@bcc/utils';
+import { T, MarkerIconPrimary, MarkerIconSuccess, MarkerIconWarning, MarkerIconDanger } from '@bcc/utils';
 
-let M;
+let M: any;
 
-const marker = new T.Marker(new T.LngLat(116.22808, 40.07779), { icon: MarkerIconPrimary });
+const markers = [
+  new T.Marker(new T.LngLat(116.22808, 40.07779), { icon: MarkerIconPrimary }),
+  new T.Marker(new T.LngLat(116.22918, 40.07811), { icon: MarkerIconSuccess }),
+  new T.Marker(new T.LngLat(116.22801, 40.07698), { icon: MarkerIconWarning }),
+  new T.Marker(new T.LngLat(116.23013, 40.07778), { icon: MarkerIconDanger })
+];
 
 onMounted(() => {
   M = new T.Map('map');
-  M.centerAndZoom(new T.LngLat(116.22808, 40.07779), 18);
-  M.addOverLay(marker);
+  M.centerAndZoom(new T.LngLat(116.22909, 40.07757), 18);
+
+  markers.forEach(marker => {
+    M.addOverLay(marker);
+  });
 });
 </script>
 
--- packages/utils/src/tianditu.ts
+++ packages/utils/src/tianditu.ts
@@ -1,9 +1,14 @@
 import markerPrimary from './assets/tianditu/marker-primary.svg';
+import markerSuccess from './assets/tianditu/marker-success.svg';
+import markerWarning from './assets/tianditu/marker-warning.svg';
+import markerDanger from './assets/tianditu/marker-danger.svg';
 
 export const T = (window as any).T;
 
-export const MarkerIconPrimary = new T.Icon({
-  iconUrl: markerPrimary,
-  iconSize: new T.Point(25, 41),
-  iconAnchor: new T.Point(13, 41)
-});
+const iconSize = new T.Point(25, 41);
+const iconAnchor = new T.Point(13, 41);
+
+export const MarkerIconPrimary = new T.Icon({ iconUrl: markerPrimary, iconSize, iconAnchor });
+export const MarkerIconSuccess = new T.Icon({ iconUrl: markerSuccess, iconSize, iconAnchor });
+export const MarkerIconWarning = new T.Icon({ iconUrl: markerWarning, iconSize, iconAnchor });
+export const MarkerIconDanger = new T.Icon({ iconUrl: markerDanger, iconSize, iconAnchor });
