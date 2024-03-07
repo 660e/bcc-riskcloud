05f9514635b193d2188f0b25b25f6eadd80eaa18
Min <solunar@vip.qq.com>
2024-03-07 12:14:32 +0800

    feat: 初始化标注列表

--- packages/ui/src/map-annotation/index.vue
+++ packages/ui/src/map-annotation/index.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup>
-import { watchEffect } from 'vue';
+import { ref, watchEffect } from 'vue';
 import { ElMessage } from 'element-plus';
 import { Map, TLngLat } from '@bcc/utils';
 
 const $props = defineProps<{
   company: any;
 }>();
+
+const active = ref(0);
 
 let M: any;
 const contextMenu = [
@@ -31,6 +33,15 @@ watchEffect(() => {
 
 <template>
   <div class="map-annotation">
+    <div class="map-annotation__points">
+      <el-tabs v-model="active" stretch>
+        <el-tab-pane label="未标注" :name="0">
+          <div style="height: 2000px">未标注</div>
+        </el-tab-pane>
+        <el-tab-pane label="已标注" :name="1">已标注</el-tab-pane>
+      </el-tabs>
+      <el-divider direction="vertical" />
+    </div>
     <div id="map"></div>
   </div>
 </template>
