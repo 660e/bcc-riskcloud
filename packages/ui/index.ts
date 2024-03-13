8b0bc09e030427c59ec6107103f8131b7538fc18
Min <solunar@vip.qq.com>
2024-03-13 21:06:12 +0800

    feat: 新增“海量点位”示例组件

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,7 +1,8 @@
+import CloudMarkerCollection from './src/cloud-marker-collection/index.vue';
 import ErrorMessage403 from './src/error-message/403.vue';
 import ErrorMessage404 from './src/error-message/404.vue';
 import ErrorMessage500 from './src/error-message/500.vue';
 import MapAnnotation from './src/map-annotation/index.vue';
 import SensitiveTargets from './src/sensitive-targets/index.vue';
 
-export { ErrorMessage403, ErrorMessage404, ErrorMessage500, MapAnnotation, SensitiveTargets };
+export { CloudMarkerCollection, ErrorMessage403, ErrorMessage404, ErrorMessage500, MapAnnotation, SensitiveTargets };
---
+++ packages/ui/src/cloud-marker-collection/index.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { onMounted } from 'vue';
+import { T, MapClass } from '@bcc/utils';
+import points from '../assets/points.json';
+
+onMounted(() => {
+  const MapUtils: MapClass = new MapClass();
+  const lnglats: any[] = points.map((point: number[]) => MapUtils.LngLat([point[0], point[1]]));
+  const M: any = MapUtils.Init('map', [108.95, 34.27], 6);
+  M.addOverLay(new T.CloudMarkerCollection(lnglats));
+});
+</script>
+
+<template>
+  <div id="map"></div>
+</template>
