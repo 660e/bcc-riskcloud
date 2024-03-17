488dce78d586817862cdea685af250b0f23d13c3
Min <solunar@vip.qq.com>
2024-03-17 18:50:13 +0800

    feat: 隐藏版权信息

--- packages/ui/src/cloud-marker-collection/index.vue
+++ packages/ui/src/cloud-marker-collection/index.vue
@@ -12,5 +12,11 @@ onMounted(() => {
 </script>
 
 <template>
-  <div id="map"></div>
+  <div class="cloud-marker-collection">
+    <div id="map"></div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
