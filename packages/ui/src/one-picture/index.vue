91ff32829d32f7df41807573ba8faf6b04267249
Min <solunar@vip.qq.com>
2024-03-21 12:34:13 +0800

    feat: 添加事件监听

--- packages/ui/src/one-picture/index.vue
+++ packages/ui/src/one-picture/index.vue
@@ -3,44 +3,54 @@ import { watch } from 'vue';
 import { MapClass } from '@bcc/utils';
 
 const $props = defineProps<{ code: any }>();
-// 行政区划信息
-let administrative: any;
 // 地图实例
 let M: any;
 // 地图工具类
 const MapUtils: MapClass = new MapClass();
 watch(
   () => $props.code,
-  async code => {
+  code => {
     if (code) {
-      administrative = await MapUtils.AdministrativeDivision(code);
-
-      if (administrative) {
-        console.log(administrative);
-
-        M = MapUtils.Init('map');
-        drawPolygon();
-      }
+      M = MapUtils.Init('map');
+      drawPolygon(code);
     }
   }
 );
 
 // 绘制区域
-const drawPolygon = () => {
+const drawPolygon = async (code: string) => {
+  const administrative: any = await MapUtils.AdministrativeDivision(code);
+
   // 当前行政区划
   administrative.geometry.coordinates.forEach((coordinate: any) => {
-    coordinate.forEach((c: any) => {
-      const polygon = MapUtils.Polygon(c, { fillOpacity: 0 });
-      M.addOverLay(polygon);
-    });
+    coordinate.forEach((c: any) => M.addOverLay(MapUtils.Polygon(c, { fillOpacity: 0 })));
   });
 
   // 下级行政区划
   administrative.child.forEach((child: any) => {
+    const polygons = {
+      adcode: child.properties.adcode,
+      name: child.properties.name,
+      data: [] as any[]
+    };
+
     child.geometry.coordinates.forEach((coordinate: any) => {
       coordinate.forEach((c: any) => {
         const polygon = MapUtils.Polygon(c, { weight: 1, lineStyle: 'dashed' });
         M.addOverLay(polygon);
+        polygons.data.push(polygon);
+      });
+    });
+
+    polygons.data.forEach((polygon: any) => {
+      polygon.addEventListener('mouseover', () => {
+        polygons.data.forEach((p: any) => p.setColor('#f00'));
+      });
+      polygon.addEventListener('mouseout', () => {
+        polygons.data.forEach((p: any) => p.setColor('#00f'));
+      });
+      polygon.addEventListener('click', () => {
+        console.log(polygons);
       });
     });
   });
