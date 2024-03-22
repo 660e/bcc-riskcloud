57bc3b1c6eb0331846fa70e4edd6be30285a14d5
Min <solunar@vip.qq.com>
2024-03-22 15:17:04 +0800

    refactor: 规范变量名

---
+++ packages/ui/src/plan-marker/index.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="plan-marker">
+    <div class="plan-marker__sidebar">
+      <div>
+        <div class="plan-marker__sources">
+          <div style="height: 2000px"></div>
+        </div>
+      </div>
+      <el-divider direction="vertical" />
+    </div>
+    <div class="plan-marker__picture"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
