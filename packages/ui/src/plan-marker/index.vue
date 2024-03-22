0838250e7f82bbe5db6ace94556d7af9d4089511
Min <solunar@vip.qq.com>
2024-03-22 16:10:58 +0800

    feat: 初始化平面图容器

--- packages/ui/src/plan-marker/index.vue
+++ packages/ui/src/plan-marker/index.vue
@@ -1,16 +1,34 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const wrapperRef = ref();
+const imgRef = ref();
+const style = ref({});
+const fit = () => {
+  const ww = wrapperRef.value.clientWidth;
+  const wh = wrapperRef.value.clientHeight;
+  const iw = imgRef.value.clientWidth;
+  const ih = imgRef.value.clientHeight;
+  style.value = ww / wh > iw / ih ? { height: '100%' } : { width: '100%' };
+};
+</script>
 
 <template>
   <div class="plan-marker">
     <div class="plan-marker__sidebar">
       <div>
-        <div class="plan-marker__sources">
-          <div style="height: 2000px"></div>
-        </div>
+        <div class="plan-marker__sources"></div>
       </div>
       <el-divider direction="vertical" />
     </div>
-    <div class="plan-marker__picture"></div>
+    <div class="plan-marker__wrapper" ref="wrapperRef">
+      <img
+        :style="style"
+        @load="fit"
+        src="https://img.zcool.cn/community/01ed1b603f20cc11013ef90f5a9146.jpg@1280w_1l_2o_100sh.jpg"
+        ref="imgRef"
+      />
+    </div>
   </div>
 </template>
 
