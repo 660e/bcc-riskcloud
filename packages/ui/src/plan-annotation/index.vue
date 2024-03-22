9ef809d38f8d58651a149bceb106812b8ced3325
Min <solunar@vip.qq.com>
2024-03-22 15:10:24 +0800

    refactor: 整理样式表

--- packages/ui/src/plan-annotation/index.vue
+++ packages/ui/src/plan-annotation/index.vue
@@ -1,5 +1,19 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>PlanAnnotation</div>
+  <div class="plan-annotation">
+    <div class="plan-annotation__sidebar">
+      <div>
+        <div class="plan-annotation__sources">
+          <div style="height: 2000px"></div>
+        </div>
+      </div>
+      <el-divider direction="vertical" />
+    </div>
+    <div class="plan-annotation__picture"></div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
