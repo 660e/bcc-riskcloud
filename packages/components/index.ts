f238769e7797d253dbaa722d703cd3e53110ffca
Min <solunar@vip.qq.com>
2024-04-02 14:42:37 +0800

    feat: 新增标签提示组件

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -3,8 +3,9 @@ export type * from './src/grid-layout/interface';
 import GridItem from './src/grid-layout/components/grid-item.vue';
 import GridLayout from './src/grid-layout/index.vue';
 import IconSelect from './src/icon-select/index.vue';
+import ImportTemplateDialog from './src/import/template.vue';
+import LabelTooltip from './src/label-tooltip.vue';
 import SvgIcon from './src/svg-icon/index.vue';
 import TreeFilter from './src/tree-filter/index.vue';
-import ImportTemplateDialog from './src/import/template.vue';
 
-export { GridItem, GridLayout, IconSelect, SvgIcon, TreeFilter, ImportTemplateDialog };
+export { GridItem, GridLayout, IconSelect, ImportTemplateDialog, LabelTooltip, SvgIcon, TreeFilter };
---
+++ packages/components/src/label-tooltip.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+defineProps<{ label: string; content: string }>();
+</script>
+
+<template>
+  <div class="label-tooltip">
+    <span>{{ label }}</span>
+    <el-tooltip :content="content" placement="top">
+      <el-icon><QuestionFilled /></el-icon>
+    </el-tooltip>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.label-tooltip {
+  display: flex;
+  align-items: center;
+  & > span {
+    margin-right: 4px;
+  }
+}
+</style>
