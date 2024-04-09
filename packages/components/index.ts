b4eca7bc68223a0b64029e58df69ccf4dfaa4502
Min <solunar@vip.qq.com>
2024-04-09 14:45:18 +0800

    refactor: 整理文件

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -2,10 +2,10 @@ export type * from './src/grid-layout/interface';
 
 import GridItem from './src/grid-layout/components/grid-item.vue';
 import GridLayout from './src/grid-layout/index.vue';
-import IconSelect from './src/icon-select/index.vue';
+import IconSelect from './src/icon-select.vue';
 import ImportTemplateDialog from './src/import/template.vue';
 import LabelTooltip from './src/label-tooltip.vue';
-import SvgIcon from './src/svg-icon/index.vue';
-import TreeFilter from './src/tree-filter/index.vue';
+import SvgIcon from './src/svg-icon.vue';
+import TreeFilter from './src/tree-filter.vue';
 
 export { GridItem, GridLayout, IconSelect, ImportTemplateDialog, LabelTooltip, SvgIcon, TreeFilter };
