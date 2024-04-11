468bfb9eb23a290ff68723907074e7f425858189
Min <solunar@vip.qq.com>
2024-04-11 15:09:01 +0800

    feat: 初始化风险分析组件

---
+++ packages/ui/src/risk-statistics/index.ts
@@ -0,0 +1,98 @@
+import { EChartsOption } from 'echarts';
+
+export const barOption = (): EChartsOption => {
+  return {
+    tooltip: {
+      trigger: 'axis'
+    },
+    legend: {},
+    grid: {
+      top: '2%',
+      left: '0',
+      right: '0',
+      bottom: '0',
+      containLabel: true
+    },
+    xAxis: [
+      {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value'
+      }
+    ],
+    series: [
+      {
+        name: 'Email',
+        type: 'bar',
+        stack: 'a',
+        emphasis: {
+          focus: 'series'
+        },
+        data: [120, 132, 101, 134, 90, 230, 210]
+      },
+      {
+        name: 'Union Ads',
+        type: 'bar',
+        stack: 'a',
+        emphasis: {
+          focus: 'series'
+        },
+        data: [220, 182, 191, 234, 290, 330, 310]
+      },
+      {
+        name: 'Video Ads',
+        type: 'bar',
+        stack: 'a',
+        emphasis: {
+          focus: 'series'
+        },
+        data: [150, 232, 201, 154, 190, 330, 410]
+      }
+    ]
+  };
+};
+
+export const pieOption = (): EChartsOption => {
+  return {
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      top: '0',
+      left: 'center'
+    },
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: ['40%', '70%'],
+        avoidLabelOverlap: false,
+        label: {
+          show: false,
+          position: 'center'
+        },
+        emphasis: {
+          label: {
+            show: true,
+            fontSize: 40,
+            fontWeight: 'bold'
+          }
+        },
+        labelLine: {
+          show: false
+        },
+        data: [
+          { value: 1048, name: 'Search Engine' },
+          { value: 735, name: 'Direct' },
+          { value: 580, name: 'Email' },
+          { value: 484, name: 'Union Ads' },
+          { value: 300, name: 'Video Ads' }
+        ]
+      }
+    ]
+  };
+};
--- packages/ui/src/risk-statistics/index.vue
+++ packages/ui/src/risk-statistics/index.vue
@@ -1,7 +1,43 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { watchEffect } from 'vue';
+import * as echarts from 'echarts';
+import { barOption, pieOption } from './index';
+
+const $props = defineProps<{ data: any }>();
+
+let pie1: any;
+let bar1: any;
+
+watchEffect(() => {
+  if ($props.data?.type) {
+    pie1 = echarts.init(document.getElementById('pie-1')!);
+    pie1.setOption(pieOption());
+
+    bar1 = echarts.init(document.getElementById('bar-1')!);
+    bar1.setOption(barOption());
+  }
+});
+</script>
 
 <template>
-  <div>
-    <div class="c-subtitle-1">单位基本信息</div>
+  <div class="risk-statistics">
+    <div>
+      <div class="c-subtitle-1">风险源等级</div>
+      <div id="pie-1" style="height: 300px; margin-top: 10px; background-color: rgba(255, 0, 0, 0.2)"></div>
+    </div>
+    <div>
+      <div class="c-subtitle-1">风险源列表</div>
+      <div id="bar-1" style="height: 300px; margin-top: 10px; background-color: rgba(255, 0, 0, 0.2)"></div>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.risk-statistics {
+  padding: 10px;
+  display: flex;
+  & > div {
+    flex: 1;
+  }
+}
+</style>
