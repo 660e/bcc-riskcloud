ac70edcc8ca8d6e98b5551b69f3ff9b3b1895e5c
Min <solunar@vip.qq.com>
2024-04-11 10:46:31 +0800

    feat: 初始化图表

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -5,5 +5,15 @@ import ErrorMessage500 from './src/error-message/500.vue';
 import MapMarker from './src/map-marker/index.vue';
 import OnePicture from './src/one-picture/index.vue';
 import PlanMarker from './src/plan-marker/index.vue';
+import RiskStatistics from './src/risk-statistics/index.vue';
 
-export { CloudMarkerCollection, ErrorMessage403, ErrorMessage404, ErrorMessage500, MapMarker, OnePicture, PlanMarker };
+export {
+  CloudMarkerCollection,
+  ErrorMessage403,
+  ErrorMessage404,
+  ErrorMessage500,
+  MapMarker,
+  OnePicture,
+  PlanMarker,
+  RiskStatistics
+};
