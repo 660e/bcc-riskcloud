50f53f558730afbe444c9f6f66d90050a956316b
Min <solunar@vip.qq.com>
2024-04-19 16:31:48 +0800

    feat: 增加风险矩阵颜色

--- packages/components/src/risk-grade-matrix.vue
+++ packages/components/src/risk-grade-matrix.vue
@@ -1,4 +1,6 @@
 <script lang="ts" name="risk-grade-matrix" setup>
+withDefaults(defineProps<{ grade: number[] }>(), { grade: () => [-1, -1] });
+
 const xData = ['很小', '小', '一般', '大', '很大'];
 const yData = [
   { td: [0, 0, 0, 1, 1], th: '基本不可能' },
@@ -7,7 +9,7 @@ const yData = [
   { td: [1, 1, 2, 2, 3], th: '较可能' },
   { td: [1, 2, 2, 3, 3], th: '很可能' }
 ];
-const grade = ['低', '一般', '较大', '重大'];
+const dict = ['低', '一般', '较大', '重大'];
 </script>
 
 <template>
@@ -25,7 +27,9 @@ const grade = ['低', '一般', '较大', '重大'];
       <tr v-for="(tr, index) in yData" :key="index">
         <th v-if="!index" rowspan="5" style="background-color: var(--el-color-warning-light-9)">可能性</th>
         <th style="background-color: var(--el-color-warning-light-9)">{{ tr.th }}</th>
-        <td v-for="(td, i) in tr.td" :key="i">{{ grade[td] }}</td>
+        <td v-for="(td, i) in tr.td" :key="i" :class="[grade[0] === index && grade[1] === i ? 'current' : '', `grade-${td}`]">
+          {{ dict[td] }}
+        </td>
       </tr>
     </tbody>
   </table>
@@ -37,9 +41,24 @@ const grade = ['低', '一般', '较大', '重大'];
   th,
   td {
     border: 1px #ccc solid;
-    padding: 10px;
+    padding: 10px 0;
     font-weight: 400;
     text-align: center;
+  }
+  td {
+    width: 80px;
+    &.grade-0.current {
+      background-color: var(--el-color-success-light-3);
+    }
+    &.grade-1.current {
+      background-color: var(--el-color-primary-light-3);
+    }
+    &.grade-2.current {
+      background-color: var(--el-color-warning-light-3);
+    }
+    &.grade-3.current {
+      background-color: var(--el-color-danger-light-3);
+    }
   }
 }
 </style>
