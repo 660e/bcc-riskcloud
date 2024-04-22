08e675a095f1f454d8e699793b2150f69259f1e8
Min <solunar@vip.qq.com>
2024-04-22 16:40:42 +0800

    chore: 迁移地图标注组件

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -2,7 +2,6 @@ import CloudMarkerCollection from './src/cloud-marker-collection/index.vue';
 import ErrorMessage403 from './src/error-message/403.vue';
 import ErrorMessage404 from './src/error-message/404.vue';
 import ErrorMessage500 from './src/error-message/500.vue';
-import MapMarker from './src/map-marker/index.vue';
 import OnePicture from './src/one-picture/index.vue';
 import PlanMarker from './src/plan-marker/index.vue';
 import RiskDetailsDialog from './src/risk-details-dialog/index.vue';
@@ -13,7 +12,6 @@ export {
   ErrorMessage403,
   ErrorMessage404,
   ErrorMessage500,
-  MapMarker,
   OnePicture,
   PlanMarker,
   RiskDetailsDialog,
